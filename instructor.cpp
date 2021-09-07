f5e38ac1b2cdbc1ed4234750d98beea545be62f8
Nathaniel <nbm54197@uga.edu>
2021-09-07 19:55:19 -0400

    instructor methods added except the average ** still need to be tested

--- instructor.cpp
+++ instructor.cpp
@@ -25,15 +25,100 @@ Instructor::Instructor(string username, string password, string firstName, strin
 } // Instructor
 
 string getInstructorName() {
+	return fName;
 } // getInstructorName
 
 Student getStudent(string username) {
+	for (int i = 0; i < 20; i++) {
+		if(!(people[i].username.compare(username))) {
+			return people[i];
+		} // if
+	} // for
+	return Student("Not","Exist", "noUN", "pw", -1, -1, -1, -1);
 } // getStudent
 
 Student getMinStudent(int gradeType) {
+	Student min = people[0];
+	switch(gradeType) {
+		case 1:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getProjectGrade() < min.getProjectGrade()) {
+					min = people[i];
+				} // if
+			} // for		
+			break;		
+		case 2:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getQuizGrade() < min.getQuizGrade()) {
+					min = people[i];
+				} // if
+			} // for		
+			break;		
+		case 3:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getMidtermGrade() < min.getMidtermGrade()) {
+					min = people[i];
+				} // if
+			} // for		
+			break;		
+		case 4:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getFinalGrade() < min.getFinalGrade()) {
+					min = people[i];
+				} // if
+			} // for		
+			break;		
+		case 5:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getOverallGrade() < min.getOverallGrade()) {
+					min = people[i];
+				} // if
+			} // for
+			break;		
+	} // switch
+	return min;
 } // getMinStudent
 
 Student getMaxStudent(int gradeType) {
+	Student max = people[0];
+	switch(gradeType) {
+		case 1:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getProjectGrade() > max.getProjectGrade()) {
+					max = people[i];
+				} // if
+			} // for		
+			break;		
+		case 2:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getQuizGrade() > max.getQuizGrade()) {
+					max = people[i];
+				} // if
+			} // for		
+			break;		
+		case 3:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getMidtermGrade() > max.getMidtermGrade()) {
+					max = people[i];
+				} // if
+			} // for		
+			break;		
+		case 4:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getFinalGrade() > max.getFinalGrade()) {
+					max = people[i];
+				} // if
+			} // for		
+			break;		
+		case 5:	
+			for (int i = 1; i < 20; i++) {
+				if (people[i].getOverallGrade() > max.getOverallGrade()) {
+					max = people[i];
+				} // if
+			} // for
+			break;		
+	} // switch
+	return max;
 } // getMaxStudent
 
 double getAvg(int gradeType) {
