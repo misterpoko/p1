098deb320143291e464ea49d9ecdf5648723998c
Nathaniel <nbm54197@uga.edu>
2021-09-07 20:40:15 -0400

    split initialize into two methods

--- main.cpp
+++ main.cpp
@@ -31,14 +31,49 @@ int main(int argc, char **argv) {
         return 0;
 } // main
 
-/**
- * initialize is going to return 1 if success and 0 if failure.
- * Failure should shortcircuit the program and cause an error.
- * @return 1 if worked and 0 if didnt initialize properly
- */
-int initialize(char *argv2, char *argv1) {
-	FILE * sFile;
-	
+int initializeInstructor(char *argv2) {
+	FILE * iFile;
+	int j = 0;
+	char c;
+	iFile = fopen(argv2, "r");
+	if (iFile == NULL) {
+		cout << "instructors.txt does not exist" << endl;
+		return 0;
+	} // if
+	string firstName;
+	string lastName;
+	string username;
+	string password;
+	string temp;
+	while (!feof(iFile)) {
+		for (int i = 0; i < 4; i++) {
+			while ((c = fgetc(iFile)) != ' ' && c != '\t' && c != '\n' && !feof(iFile)) {
+				temp = temp + c;
+			} // while
+			switch(i) {
+				case 0:
+					username = temp;
+					break;
+				case 1:
+					password = temp;
+					break;
+				case 2: 
+					firstName = temp;
+					break;
+				case 3:
+					lastName = temp;
+					break;
+			} // switch
+			temp = "";
+		} // for
+		//add instructor constructor lol
+	} // while
+	fclose(iFile);
+	return 1;
+} // initializeInstructor
+
+int initializeStudent(char *argv1) {
+	FILE * sFile;	
 	sFile = fopen(argv1, "r");
 	if (sFile == NULL) {
 		cout << "students.txt does not exist" << endl;
@@ -90,37 +125,22 @@ int initialize(char *argv2, char *argv1) {
 		} // for
 		theStudents[j++] = Student(firstName, lastName, username, password, finalGrade, midtermGrade, quizGrade, projectGrade);
 	} // while
-	fclose(sFile);	
-	j = 0;
-	sFile = fopen(argv2, "r");
-	if (sFile == NULL) {
-		cout << "instructors.txt does not exist" << endl;
+	fclose(sFile);
+	return 1;
+} // initializeStudent
+
+/**
+ * initialize is going to return 1 if success and 0 if failure.
+ * Failure should shortcircuit the program and cause an error.
+ * @return 1 if worked and 0 if didnt initialize properly
+ */
+int initialize(char *argv2, char *argv1) {
+	if (!initializeStudent(argv1)) {
 		return 0;
 	} // if
-	while (!feof(sFile)) {
-		for (int i = 0; i < 4; i++) {
-			while ((c = fgetc(sFile)) != ' ' && c != '\t' && c != '\n' && !feof(sFile)) {
-				temp = temp + c;
-			} // while
-			switch(i) {
-				case 0:
-					username = temp;
-					break;
-				case 1:
-					password = temp;
-					break;
-				case 2: 
-					firstName = temp;
-					break;
-				case 3:
-					lastName = temp;
-					break;
-			} // switch
-			temp = "";
-		} // for
-		//add instructor constructor lol
-	} // while
-	fclose(sFile);
+	if (!initializeInstructor(argv2)) {
+		return 0;
+	} // if
 	return 1;
 } // initialize
 
